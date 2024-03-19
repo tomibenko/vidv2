90931ef7f1d96c7c15c0e33a5b7464cfc95960a8
tomibenko <tomi.benko@student.um.si>
2024-03-19 14:21:02 +0100

    prvi commit

--- vaja1.py
+++ vaja1.py
@@ -1,10 +1,37 @@
 import cv2 as cv
 import numpy as np
 
+
 def konvolucija(slika, jedro):
-    '''Izvede konvolucijo nad sliko. Brez uporabe funkcije cv.filter2D, ali katerekoli druge funkcije, ki izvaja konvolucijo.
-    Funkcijo implementirajte sami z uporabo zank oz. vektorskega računanja.'''
-    pass
+        # Določite dimenzije vhodne slike in jedra
+    if len(slika.shape) == 3:  # Če je slika večkanalna (npr. RGB), vzamemo samo prvi kanal
+        image_height, image_width, num_channels = slika.shape
+    else:
+        image_height, image_width = slika.shape
+        num_channels = 1
+    
+    # Določite dimenzije jedra
+    kernel_height, kernel_width = jedro.shape
+    
+    # Nastavite padding, da se izognete izgubi robov pri konvoluciji
+    padding_height = kernel_height // 2
+    padding_width = kernel_width // 2
+    
+    # Ustvarite izhodno sliko
+    output = np.zeros_like(slika)
+    
+    # Izvedite konvolucijo
+    for i in range(padding_height, image_height - padding_height):
+        for j in range(padding_width, image_width - padding_width):
+            # Izračunajte vrednost konvolucije za trenutno pozicijo
+            sum = 0
+            for m in range(kernel_height):
+                for n in range(kernel_width):
+                    sum += jedro[m, n] * slika[i - padding_height + m, j - padding_width + n]
+            output[i, j] = sum
+    
+    return output
+    
 
 def filtriraj_z_gaussovim_jedrom(slika,sigma):
     '''Filtrira sliko z Gaussovim jedrom..'''
@@ -13,6 +40,14 @@ def filtriraj_z_gaussovim_jedrom(slika,sigma):
 def filtriraj_sobel_smer(slika):
     '''Filtrira sliko z Sobelovim jedrom in označi gradiente v orignalni sliki glede na ustrezen pogoj.'''
     pass
-
+  
 if __name__ == '__main__':    
+    jedro = np.array([[1, 1, 1],
+                     [1, 1, 1],
+                     [1, 1, 1]])
+    slika = cv.imread('lenna.png')
+    slike=konvolucija(slika,jedro)
+    cv.imshow("neke",slika)
+    cv.waitKey(0)
+   
     pass
